9c3e633314706d818cba466c3263e8ceccb47d4e
unknown <tsmith110179@yahoo.com>
2024-09-01 16:42:23 -0500

    App.tsx - part 2

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,20 @@ const audioClips: AudioClip[] =[
   },
 ]
 
+
 function App() {
+
+  const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    const clip = audioClips.find(
+      (clip) => clip.keyTrigger === e.key.toUpperCase()
+    );
+    if (!clip) return;
+    document.getElementById(clip.keyTrigger as HTMLAudioElement).play().catch(console.error);
+
+    document.getElementById('drum-' + clip.keyTrigger)?.focus();
+    document.getElementById('display')!.innerText = clip.description;
+}
+/*function App() {
 
   const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) => {
     const clip = audioClips.find(
@@ -63,13 +76,13 @@ function App() {
    /* .then(() => {
       console.log('played');
     }))*/
-    .catch(console.error);
+    /*.catch(console.error);
 
     document.getElementById('drum-' + clip.keyTrigger)?.focus();
     document.getElementById('display')!.innerText = clip.description;
 };
 
-
+*/
 return (
   <div className="container" id="drum-machine" onKeyDown={playAudio}>
     <h1>Trent's Drum Machine</h1>
