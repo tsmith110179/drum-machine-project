29cf1bfa4058ddf2937c52fc6bdf421232a4b113
unknown <tsmith110179@yahoo.com>
2024-09-01 16:16:21 -0500

    Full Project

---
+++ src/Drum.tsx
@@ -0,0 +1,25 @@
+import { AudioClip} from './types.ts';
+
+interface DrumProps{
+    audioClip: AudioClip;
+}
+
+const DrumPad = ({audioClip}: DrumPadProps) => {
+    const playSound = (clip: AudioClip) => {
+        (document.getElementById(clip.keyTrigger) as HTMLAudioElement)
+            .play()
+            .then(() => {
+                console.log('played');
+              })
+            .catch(console.error);
+        document.getElementById('display')!.innerText = clip.description;
+    };
+    return (
+       <button className="drum-pad" id={`drum-${audioClip.keyTrigger}`} onClick={() => playSound(audioClip)}>
+        <audio src={audioClip.url} id={audioClip.keyTrigger} className="clip" />
+        {audioClip.keyTrigger}
+       </button>
+    )
+}
+
+export default DrumPad
