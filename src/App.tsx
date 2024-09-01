ed625db2a216f3fd7176a201adc0fc5e14b47370
unknown <tsmith110179@yahoo.com>
2024-09-01 16:46:28 -0500

    Fourth Try

--- src/App.tsx
+++ src/App.tsx
@@ -52,37 +52,24 @@ const audioClips: AudioClip[] =[
   },
 ]
 
-
 function App() {
-
-  const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    const clip = audioClips.find(
-      (clip) => clip.keyTrigger === e.key.toUpperCase()
-    );
-    if (!clip) return;
-    document.getElementById(clip.keyTrigger as HTMLAudioElement).play().catch(console.error);
-
-    document.getElementById('drum-' + clip.keyTrigger)?.focus();
-    document.getElementById('display')!.innerText = clip.description;
-}
-/*function App() {
 
   const playAudio = (e: React.KeyboardEvent<HTMLDivElement>) => {
     const clip = audioClips.find(
       (clip) => clip.keyTrigger === e.key.toUpperCase());
     if (!clip) return;
-    document.getElementById(clip.keyTrigger as HTMLAudioElement)
+    (document.getElementById(clip.keyTrigger as HTMLAudioElement)
     .play()
-   /* .then(() => {
+    .then(() => {
       console.log('played');
-    }))*/
-    /*.catch(console.error);
+    }))
+    .catch(console.error);
 
     document.getElementById('drum-' + clip.keyTrigger)?.focus();
     document.getElementById('display')!.innerText = clip.description;
 };
 
-*/
+
 return (
   <div className="container" id="drum-machine" onKeyDown={playAudio}>
     <h1>Trent's Drum Machine</h1>
